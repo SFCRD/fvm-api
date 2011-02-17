fba611052c94984fe49adf05b4e65cdaf77b9840
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-17 10:17:56 -0800

    hello git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FvmApi::Application.config.secret_token = '1133d0ade5ed066e6d3460ecd2eba7965d12c165b6ccc2f7557dca76ef22cd21e93e070e424c7061192f405419a0e8147d5349a14d2468b460caf08e4418dc4a'
