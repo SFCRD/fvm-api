cb1506a7484ef71a874873505cdcf224d8fd8d3c
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2011-02-17 16:17:57 -0800

    all green

--- test/functional/builds_controller_test.rb
+++ test/functional/builds_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class BuildsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
+  test "should get index json" do
+    get :index, :format => :json
     assert_response :success
   end
   
+  test "should get index xml" do
+    get :index, :format => :xml
+    assert_response :success
+  end
 end
--- test/unit/build_worker_test.rb
+++ test/unit/build_worker_test.rb
@@ -8,11 +8,22 @@ class BuildWorkerTest < Test::Unit::TestCase
   end
   # flattens tables into an array
   def test_flattens_tables_into_an_array
-    assert_instance_of( Array, @worker.flatten_tables( @data ) )
+    assert_instance_of( Array, @worker.flatten( 'sdk name', @data ) )
   end
   # flattened array has correct length
   def test_flattened_array_has_correct_length
-    assert_equal( 4, @worker.flatten_tables( @data ).size )
+    assert_equal( 4, @worker.flatten( 'sdk name', @data ).size )
   end
-  
+  # objects in flattened array have sdk property
+  def test_objects_in_flattened_array_have_sdk_property
+    @worker.flatten( 'sdk name', @data ).each do |build|
+      assert_not_nil( build[ 'SDK' ] )
+    end
+  end
+  # object in flattened array have correct sdk property
+  def test_object_in_flattened_array_have_correct_sdk_property
+    @worker.flatten( 'sdk name', @data ).each do |build|
+      assert_equal( 'sdk name', build[ 'SDK' ] )
+    end
+  end
 end
